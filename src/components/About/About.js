f5c9d98f92795d7dd1f5b7b0dde2c636c28290af
Kyle Strout <62024550+KyleStrout@users.noreply.github.com>
2022-01-24 20:26:39 -0500

    Skeleton website with routing

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+    <React.Fragment>
+        <div className="About">
+            <h1>About</h1>
+
+        </div>
+    </React.Fragment>
+}
+
+export default About;
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { makeStyles, themeProvider } from "@mui/material";
+import { Button } from "@mui/material";
+
+function LandingPage(props) {
+  return (
+    <React.Fragment>
+      <div>
+        <h1>Landing Page</h1>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default LandingPage;
