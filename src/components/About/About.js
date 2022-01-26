2f32af17a001df7f8ab67341487fd628afa1251e
Kyle Strout <62024550+KyleStrout@users.noreply.github.com>
2022-01-25 21:44:37 -0500

    sidebar

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import SideNav from "../SideNav";
 
 function About() {
   return (
     <div>
       <Container>
         <Typography variant="h2">About</Typography>
+        <SideNav />
       </Container>
     </div>
   );
