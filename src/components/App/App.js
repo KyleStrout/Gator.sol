416c358cd860f82ffb3f64334045e0f92bd9c047
Jack Driscoll <jdriscoll98@ufl.edu>
2022-01-27 23:44:13 +0000

    replace box with container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import React from "react";
 import { ThemeProvider } from "@mui/styles";
 import { createTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "../Navbar";
 import CourseHome from "../CourseHome";
@@ -22,30 +23,18 @@ const theme = createTheme({
 
 function App() {
   return (
-    <Box
-      sx={{
-        width: "100%",
-        height: "100%",
-      }}
-    >
+    <Container fluid disableGutters>
       <ThemeProvider theme={theme}>
         <Navbar />
 
-        <Box
-          sx={{
-            height: "calc(100% - 4rem)",
-            width: "100%",
-          }}
-        >
-          <Routes>
-            <Route exact path="/" element={<LandingPage />}></Route>
-            <Route path="/about" element={<About />}></Route>
-            <Route path="/home" element={<LandingPage />}></Route>
-            <Route path="/course-home" element={<CourseHome />}></Route>
-          </Routes>
-        </Box>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/home" element={<LandingPage />}></Route>
+          <Route path="/course-home" element={<CourseHome />}></Route>
+        </Routes>
       </ThemeProvider>
-    </Box>
+    </Container>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,33 +1,28 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   let navigate = useNavigate();
   return (
-    <Box sx={{ flexGrow: 1, height: "4rem" }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Button
-              onClick={() => {
-                navigate("/home");
-              }}
-              variant="text"
-              color="inherit"
-            >
-              Blockchain Education
-            </Button>
-          </Typography>
-          <Button color="inherit">Connect to Wallet</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="static" sx={{ height: "4rem" }}>
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Button
+            onClick={() => {
+              navigate("/home");
+            }}
+            variant="text"
+            color="inherit"
+          >
+            Blockchain Education
+          </Button>
+        </Typography>
+        <Button color="inherit">Connect to Wallet</Button>
+      </Toolbar>
+    </AppBar>
   );
 }
