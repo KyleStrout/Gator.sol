ee305aaa39ed40a854edeb697ec9eb34c1b4c6a4
Jack Driscoll <jdriscoll98@ufl.edu>
2022-04-04 22:05:56 -0400

    wip

--- src/components/ThemeContext/index.js
+++ src/components/ThemeContext/index.js
@@ -8,7 +8,7 @@ export const themes = {
     textColor: "black",
     fontColor: "black",
     topBar: "#1976d2",
-    compileButton: "#9c27b0",
+    compileButton: "#0000FF",
     deployButton: "#A4A4A4",
     buttonsbackground: "white",
     landingPageBigBox: "gray",
@@ -47,16 +47,16 @@ export const themes = {
   },
   dark: {
     codeEditor: "vs-dark",
-    backgroundColor: "gray",
-    backgroundColorSecondary: "black",
-    textColor: "white",
-    fontColor: "white",
-    topBar: "#003366",
+    backgroundColor: "#202124",
+    backgroundColorSecondary: "#2E3134",
+    textColor: "#F8F9FA",
+    fontColor: "#F8F9FA",
+    topBar: "#OE1013",
     compileButton: "#5d3264",
     deployButton: "black",
     buttonsbackground: "gray",
     landingPageBigBox: "white",
-    sideNavAccordion: "gray",
+    sideNavAccordion: "#2E3134",
     border: "0.5rem solid #000000",
   },
 };
@@ -75,7 +75,11 @@ export const sessionStorage =
       };
 
 const getTheme = (_mode) => {
-  const mode = _mode ? sessionStorage.getItem("themeMode") : "light";
+  const mode = _mode
+    ? _mode
+    : sessionStorage.getItem("themeMode")
+    ? sessionStorage.getItem("themeMode")
+    : "light";
 
   return responsiveFontSizes(
     createTheme({
