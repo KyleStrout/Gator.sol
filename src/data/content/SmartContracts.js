4100a19771d983903b6ed3d91c17a34633b07362
colinnaehr <cnaehr@ufl.edu>
2022-04-17 16:50:46 -0400

    Smart contract content

---
+++ src/data/content/SmartContracts.js
@@ -0,0 +1,23 @@
+export const content = `
+# What is a Smart Contract?
+
+Smart contracts are a collection of programs that are stored on the blockchain, and they run under 
+certain conditions. When they run, the action takes place across a network of computers. One benefit 
+of smart contracts is that they are automated; this means that whenever the predetermined conditions 
+are met, the process will instantly occur. Smarts contracts make it so that a middle-man is no longer 
+needed, and give blockchain's various uses more security. Finally smart contracts are immutable, 
+which means they can not be changed. Once the contract is coded, it is there forever. In this course, 
+we will look at smart contracts as the pertain to the Solidity.
+
+For more information, please watch the video below to gain an enhanced overview on smart contracts.
+
+## Smart Contracts - Simply Explained:
+<div style= "height: 450px">
+<iframe width="560" height="315"
+src="https://www.youtube.com/embed/ZE2HxTmxfrI" 
+frameborder="0" 
+allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+allowfullscreen></iframe>
+</div>
+
+`;
