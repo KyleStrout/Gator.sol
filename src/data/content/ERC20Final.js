783381d603340d70ca319927c7ffaf3b26e9db21
Jack Driscoll <jdriscoll98@ufl.edu>
2022-04-19 11:46:29 -0400

    final page

---
+++ src/data/content/ERC20Final.js
@@ -0,0 +1,17 @@
+export const content = `## ERC20 - Wrapping up!
+
+#### Congrats! You can now deploy and use your own ERC20 token!
+
+To see this token in meta mask, complete the following steps. 
+
+1. Click 'compile'
+2. Click 'Deploy'
+3. Wait for the contracts to be deployed
+4. Open metamask, click on the "assets" tab and scroll down to click on "import tokens";
+5. Copy the contract address you just deployed called "ERC20" and paste that into the "Contract Address" field of the import tokens form.
+6. It should auto fill in the "Token Name" and "Token Symbol" fields.
+7. Once you can see your token in the assets tab, move on to the next step. 
+8. Open the interaction panel and paste 1000000000000000000 (this is based off of 18 decimals) into the 'amount' field of the 'mint' function
+9. Confirm the transaction, and then open metamask.
+10. You should see your token in the assets tab with how ever much you minted.
+`;
